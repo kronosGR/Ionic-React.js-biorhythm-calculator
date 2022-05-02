06287d1ebc87893ca3110d5c7d5ee2a95003ce69
kronosGR <geo.elgeo@gmail.com>
2022-05-02 14:27:40 +0200

    added day.js

--- src/components/BiorhythmCard.js
+++ src/components/BiorhythmCard.js
@@ -1,11 +1,16 @@
 import { IonCard, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/react';
+import dayjs from 'dayjs';
 import React from 'react';
+
+function formatDate(isoString) {
+  return dayjs(isoString).format('D MMM YYYY');
+}
 
 export default function BiorhythmCard({ targetDate }) {
   return (
     <IonCard className='ion-text-center'>
       <IonCardHeader>
-        <IonCardTitle>{targetDate}</IonCardTitle>
+        <IonCardTitle>{formatDate(targetDate)}</IonCardTitle>
         <IonCardContent>
           <p>Physical: 80%</p>
           <p>Emotional: 80%</p>
