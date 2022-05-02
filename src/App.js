594d84307dddabea8cbe601ce1013ce25c4775c2
kronosGR <geo.elgeo@gmail.com>
2022-05-02 08:44:21 +0200

    added iondatetime

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import {
   IonInput,
   IonItem,
   IonLabel,
+  IonDatetime,
 } from '@ionic/react';
 import React, { useState } from 'react';
 
 function App() {
   const [name, setName] = useState('');
+  const [birthday, setBirthday] = useState('2020-01-31');
 
   return (
     <IonApp>
@@ -27,9 +29,13 @@ function App() {
         </IonItem>
         <IonItem>
           <IonLabel position='floating'>Date of birth:</IonLabel>
-          <IonInput />
+          <IonDatetime
+            displayFormat='D MMM YYYY'
+            onIonChange={(event) => setBirthday(event.detail.value)}
+          />
         </IonItem>
         <p>Name {name}</p>
+        <p>Birthday {birthday}</p>
       </IonContent>
     </IonApp>
   );
