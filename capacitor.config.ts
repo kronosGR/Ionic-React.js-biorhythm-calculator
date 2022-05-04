05b552d8faa3121ef46b070075027db6cfeddf82
kronosGR <geo.elgeo@gmail.com>
2022-05-04 08:05:08 +0200

    edit config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,10 @@ const config: CapacitorConfig = {
   appId: 'me.kandz.biorhythms',
   appName: 'Biorhythms',
   webDir: 'build',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  server: {
+    url: 'http://192.168.1.70:3000',
+  },
 };
 
 export default config;
